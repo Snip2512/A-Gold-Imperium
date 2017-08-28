d41d34ee0de2a9dde0abd4a4efb42e66f3c58470
Snip2512 <snipeman2512@hotmail.com>
2017-08-28 15:40:04 +0100

    Added  Stats button

---
+++ Assets/Scripts/UI/UIStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//Monster Class to hold Button functionality on the UI.
+
+
+public class UIStats : MonoBehaviour {
+
+    GameObject testText;
+
+    // Use this for initialization
+    void Start () {
+        testText = GameObject.Find("TestText");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowStats() {       
+        
+        if (testText.activeSelf == true) {
+            testText.SetActive(false);
+        }else {
+            testText.SetActive(true);
+        }
+
+
+    }
+}
